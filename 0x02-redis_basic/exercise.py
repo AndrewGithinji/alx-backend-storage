9baf33a74f08621c8a509696ceddb774534113b1
AndrewGithinji <andrewgithinji@hotmail.com>
2023-05-17 09:41:04 +0300

    Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,3 +6,16 @@ from uuid import uuid4
 from typing import Union, Callable
 from functools import wraps
 import redis
+
+def count_calls(method: Callable = None) -> Callable:
+    """ Decorator count calls """
+    name = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """ Wrapper method """
+        self._redis.incr(name)
+        result = method(self, *args, **kwargs)
+        return result
+
+    return wrapper
